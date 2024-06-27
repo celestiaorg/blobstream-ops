31627cc30eda372cecb28bb5f756a5435b049736
sweexordious <chamirachid1@gmail.com>
2024-06-27 15:13:54 +0100

    feat: add the verification command

---
+++ cmd/blobstream-ops/root/cmd.go
@@ -0,0 +1,26 @@
+package root
+
+import (
+	"github.com/celestiaorg/blobstream-ops/cmd/blobstream-ops/verify"
+	"github.com/celestiaorg/blobstream-ops/cmd/blobstream-ops/version"
+	"github.com/spf13/cobra"
+)
+
+// Cmd creates a new root command for the Blobstream-ops CLI. It is called once in the
+// main function.
+func Cmd() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:          "blobstream-ops",
+		Short:        "The Blobstream OPS CLI",
+		SilenceUsage: true,
+	}
+
+	rootCmd.AddCommand(
+		version.Cmd,
+		verify.Command(),
+	)
+
+	rootCmd.SetHelpCommand(&cobra.Command{})
+
+	return rootCmd
+}
